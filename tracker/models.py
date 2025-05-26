c57d3a8bed05c440967d294488cc0029427c605e
Bartłomiej 'PokerFacowaty' Nowak <pokerfacowaty.bn@gmail.com>
2025-05-26 22:08:23 +0200

    Added initial models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.TextField()
+    device = models.OneToOneField("SOSDevice", null=True,
+                                  on_delete=models.SET_NULL)
+
+
+class SOSDevice(models.Model):
+    device_id = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    timestamp = models.DateTimeField()
