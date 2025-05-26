3368d3f63116ef0424d23a0b778ceddca2952e3d
Bartłomiej 'PokerFacowaty' Nowak <pokerfacowaty.bn@gmail.com>
2025-05-26 22:26:23 +0200

    Finalized models and added migrations

--- tracker/models.py
+++ tracker/models.py
@@ -2,13 +2,15 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.TextField()
-    device = models.OneToOneField("SOSDevice", null=True,
-                                  on_delete=models.SET_NULL)
+    name = models.CharField(max_length=255)
 
 
 class SOSDevice(models.Model):
-    device_id = models.TextField()
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    timestamp = models.DateTimeField()
+    device_id = models.CharField(unique=True, max_length=255)
+    user = models.OneToOneField(User, null=True,
+                                on_delete=models.SET_NULL,
+                                related_name="device")
+    # Nulls for initially added devices that haven't sent anything yet.
+    latitude = models.FloatField(null=True)
+    longitude = models.FloatField(null=True)
+    timestamp = models.DateTimeField(null=True)
